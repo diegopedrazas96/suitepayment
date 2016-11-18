72479f6300c1c9dd956496a577037bc891a97597
Diego Pedrazas <pedrazasdiegostanley96@gmail.com>
2016-11-18 12:00:47 -0400

    18/11/2016 Desde Avatech

---
+++ app/src/main/java/com/megasystem/suitepayment/activity/Clasificadores.java
@@ -0,0 +1,15 @@
+package com.megasystem.suitepayment.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.megasystem.suitepayment.R;
+
+public class Clasificadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clasificadores);
+
+    }
+}
---
+++ app/src/main/java/com/megasystem/suitepayment/activity/Gastos.java
@@ -0,0 +1,14 @@
+package com.megasystem.suitepayment.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.megasystem.suitepayment.R;
+
+public class Gastos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gastos);
+    }
+}
